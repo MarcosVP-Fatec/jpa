73a58f7e4bd25322780ba56a5e4f05432fcbde6d
MarcosVP-Fatec <marcos.pereira47@fatec.sp.gov.br>
2022-01-03 00:31:16 +0000

    Aula 04 - Criteria Query

--- Reset.sql
+++ Reset.sql
@@ -1,2 +1,7 @@
+create schema if not exists cadastroclientes;
+create schema if not exists cadastrousuarios;
+
 create user if not exists 'root'@'localhost' identified by 'root';
-grant select, insert, delete, update on *.* to root@'localhost';
+
+grant select, insert, delete, update on cadastroclientes.* to root@'localhost';
+grant select, insert, delete, update on cadastrousuarios.* to root@'localhost';
